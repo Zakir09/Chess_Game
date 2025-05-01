f6657f1d9d8348abc44888bd673facf395027ed3
Zakir <zakirahmed2002@hotmail.com>
2025-05-01 13:31:49 +0100

    Commit with project structure, logic, UI, and assets

---
+++ Chess/ChessLogic/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        Black,
+        White
+    }
+
+    public static class PlayerExtentions
+    {
+        public static Player Opponent(this Player player)
+        {
+            return player switch
+            {
+                Player.Black => Player.White,
+                Player.White => Player.Black,
+                _ => Player.None,
+            };
+        }
+    }
+}
